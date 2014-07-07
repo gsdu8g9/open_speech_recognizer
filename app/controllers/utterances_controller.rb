959a0aaa592719667b86a2ef92d12a313473cd1a
Prelang Builder <builder@prelang.com>
2014-07-07 00:39:38 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/utterances_controller.rb
+++ app/controllers/utterances_controller.rb
@@ -1,4 +1,8 @@
 class UtterancesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_utterance, only: [:show, :edit, :update, :destroy]
 
   # GET /utterances
