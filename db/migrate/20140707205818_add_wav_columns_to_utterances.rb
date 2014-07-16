17689f99e795114a89cf8a773df8a33fd9c01784
pbrusco <pablo.brusco@gmail.com>
2014-07-16 14:29:21 -0300

    wav upload using carrierwave working

--- db/migrate/20140707205818_add_wav_columns_to_utterances.rb
+++
@@ -1,9 +0,0 @@
-class AddWavColumnsToUtterances < ActiveRecord::Migration
-  def self.up
-    add_attachment :utterances, :wav
-  end
-
-  def self.down
-    remove_attachment :utterances, :wav
-  end
-end
